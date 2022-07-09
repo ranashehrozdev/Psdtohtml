28dbc330516109f52430074209c7fcc4bdd437fc
Rana Shehroz <66749505+ranashehrozdev@users.noreply.github.com>
2022-07-09 14:08:37 +0500

    done

---
+++ script.js
@@ -0,0 +1,35 @@
+  
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var country = btnTarget.dataset.country;
+
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + country).classList.add("active");
+   
+   btnTarget.classList.add("active");
+}
+
+function myFunction() {
+   var x = document.getElementById("mnavbar");
+   if (x.className === "navbar") {
+     x.className += " responsive";
+   } else {
+     x.className = "navbar";
+   }
+ }
