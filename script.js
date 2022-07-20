36539f2aba01eafab0a834381c47d035773f7bf6
Rana Shehroz <66749505+ranashehrozdev@users.noreply.github.com>
2022-07-21 03:54:36 +0500

    updated mob setting

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-  
+// $(document).ready(function(){
+
+//    //hamburger Toggle
+//    $('.humbarger').click(function(event){
+//    $('.menu-list').slideToggle(500);
+//    event.preventDefault();
+   
+//    $('.menu-list li a').click(function(event) {
+//        if ($(window).width() < 768) {
+//          $('.menu-list').slideUp(500);
+//          event.preventDefault(); 
+//        }
+//      });
+// });
+
+// });
+
+function myFunction() {
+   var x = document.getElementById("mnavbar");
+   if (x.className === "naavbar") {
+      x.className += " responsive";
+   } else {
+      x.className = "naavbar";
+   }
+}
+
+let changeIcon = function (icon) {
+   icon.classList.toggle("fa-times");
+}
+
 var tabLinks = document.querySelectorAll(".tablinks");
 var tabContent = document.querySelectorAll(".tabcontent");
 
 
-tabLinks.forEach(function(el) {
+tabLinks.forEach(function (el) {
    el.addEventListener("click", openTabs);
 });
 
@@ -12,24 +41,15 @@ function openTabs(el) {
    var btnTarget = el.currentTarget;
    var country = btnTarget.dataset.country;
 
-   tabContent.forEach(function(el) {
+   tabContent.forEach(function (el) {
       el.classList.remove("active");
    });
 
-   tabLinks.forEach(function(el) {
+   tabLinks.forEach(function (el) {
       el.classList.remove("active");
    });
 
    document.querySelector("#" + country).classList.add("active");
-   
+
    btnTarget.classList.add("active");
 }
-
-function myFunction() {
-   var x = document.getElementById("mnavbar");
-   if (x.className === "navbar") {
-     x.className += " responsive";
-   } else {
-     x.className = "navbar";
-   }
- }
